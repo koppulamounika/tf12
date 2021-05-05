57dc22f7c159f41d24a7f993c29acc504ff72f88
koppula <mounica.koppula@gmail.com>
2021-05-05 15:41:30 +0530

    added

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "StorageAccount-ResourceGroup"
-    storage_account_name = "teststroage123"
-    container_name       = "tfstate"
-    key                  = "eGM5921m162YokxYYdUaRawFcJh+0zV/MA2tw0wxliyQwRY9LdG9YUz7HQLvMTaEOdjDzGFCntlx/E62cBLFVg=="
-  }
-}
